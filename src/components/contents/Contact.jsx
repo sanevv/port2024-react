3ab32b059c4cb3384366d796014be78c0d924265
sanhae.kang <sanhae.kang@barunn.net>
2024-02-19 17:21:10 +0900

    연락처 영역

--- src/components/contents/Contact.jsx
+++ src/components/contents/Contact.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import { contactText } from '../../data/contact';
 
 const Contact = () => {
   return (
-    <div>
-      Contact
-    </div>
+    <section id="contact">
+        <div className="contact__inner">
+            <h2 className="contact__title">Contact</h2>
+            <div className="contact__lines" aria-hidden="true">
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+            </div>
+            <div className="contact__text">
+              <div className="text">
+                  {contactText.map((contact, key) => (
+                      <div key={key}>
+                          <a
+                              href={contact.link}
+                              rel="noopener noreferrer"
+                              target={contact.blank}
+                          >
+                              {contact.title}
+                          </a>
+                      </div>
+                  ))}
+              </div>
+            </div>
+            <div className="contact__lines bottom" aria-hidden="true">
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+            </div>
+        </div>
+    </section>
   );
 };
 
---
+++ src/data/contact.js
@@ -0,0 +1,13 @@
+//export 내보내기 선언을 해야 다른 페이지에서 사용 가능
+export const contactText = [
+    {
+        link: "https://open.kakao.com/o/gM7YLzwf",
+        title: "KAKAO : webstupids",
+        blank:"_blank",
+    },
+    {
+        link: "mailto:webstoryboy@naver.com",
+        title: "mail : webstoryboy@naver.com",
+        blank:"_blank",
+    },
+];
