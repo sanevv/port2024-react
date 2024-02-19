29c9d9e7489e94cf70731747f124fe268f7e4adf
sanhae.kang <sanhae.kang@barunn.net>
2024-02-19 16:41:33 +0900

    스킬페이지

--- src/components/contents/Skill.jsx
+++ src/components/contents/Skill.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { skillText } from '../../data/skill';
 
 const Skill = () => {
   return (
-    <div>
-      Skill
-    </div>
+    <section id="skill">
+        <div className="skill__inner">
+            <h2 className="skill__title">
+                Challenge <em>나의 도전</em>
+            </h2>
+            <div className="skill__desc">
+                {skillText.map((skill, key) => (
+                    <div key={key}>
+                        <span>{key + 1}.</span>
+                        <h3>{skill.title}</h3>
+                        <p>{skill.desc}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    </section>
   );
 };
 
--- src/data/intro.js
+++ src/data/intro.js
@@ -1,4 +1,3 @@
-
 //export 내보내기 선언을 해야 다른 페이지에서 사용 가능
 export const introText = {
     title: "Developer",
